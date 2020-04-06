c60b7fe371af9188f8f7ef2b4423a2d19cd2d620
auusui <60081732+auusui@users.noreply.github.com>
2020-04-05 20:45:56 -1000

    Task 3

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Container, Header } from 'semantic-ui-react';
 
 class Laundromat extends React.Component {
 
   render() {
     return (
-        <h1>Kapahulu Express Laundromat</h1>
-  );
+        <Container textAlign="center">
+          <Header as='h1'>Kapahulu Express Laundromat</Header>
+        </Container>
+    );
   }
 }
 
-ReactDOM.render(<Laundromat />, document.getElementById('root'));
+ReactDOM.render(<Laundromat/>, document.getElementById('root'));
